a762f322d8b45cac3b9221d6e26487498a1be035
Paul <pauldavis101@gmail.com>
2019-10-03 18:20:09 -0500

    Final build, generated xml matches provided examples

--- src/JackTokenizer.java
+++ src/JackTokenizer.java
@@ -26,6 +26,7 @@ public class JackTokenizer {
         symbols.add('~');
         symbols.add('<');
         symbols.add('>');
+        symbols.add(';');
     }
 
     // Reserved Words
@@ -54,6 +55,9 @@ public class JackTokenizer {
         reservedWords.add("this");
     }
 
+    // Boolean to hold current comment reading state
+    private static boolean readingComment = false;
+
     // Input file converted to queue of lines
     private static List<String> inputFileQueue = new ArrayList<>();
 
@@ -74,11 +78,6 @@ public class JackTokenizer {
             e.printStackTrace();
         }
 
-
-        // TODO: Analyze
-
-
-
         // Write Output
         // Write root tag
         writeBuffer.add('<');
@@ -89,10 +88,8 @@ public class JackTokenizer {
         writeBuffer.add('>');
         writeBuffer.add('\n');
 
-
-        // TODO: Write analyzed
-        writeTag("constant", "Hello");
-
+        for(String currentLine : inputFileQueue)
+            analyzeLine(currentLine);
 
         writeBuffer.add('<');
         writeBuffer.add('/');
@@ -108,6 +105,166 @@ public class JackTokenizer {
         }
     }
 
+    private static void analyzeLine(String line) {
+        String currentToken = "", tokenType = "";
+        boolean writeToken = false, checkNext = false;
+
+        for(Character lexeme : line.toCharArray()) {
+
+            /***********************************************************************************************************
+             * Comments                                                                                                *
+             ***********************************************************************************************************/
+            // Currently running comment check
+            if(readingComment) {
+                currentToken += lexeme;
+                // We've been told to check next, if it fits bill reset state and move on
+                if(checkNext && lexeme == '/') {
+                    readingComment = false;
+                    currentToken = tokenType = "";
+                    writeToken = checkNext = false;
+                    continue;
+                }
+                checkNext = lexeme == '*';
+                continue;
+            }
+
+            // Handle Comments start
+            // Haven't started a new token and starts with /
+            if(currentToken.equals("") && lexeme == '/') {
+                currentToken += lexeme;
+                continue;
+            }
+            // Single /, check for start of comment state
+            if(currentToken.equals("/")) {
+                // Go to end of line, we can skip the entire line at this point
+                if(lexeme == '/') {
+                    return;
+                }
+                else if(lexeme == '*') {
+                    // Starting block comment
+                    readingComment = true;
+                    currentToken += lexeme;
+                    continue;
+                } else { // We are just a divide symbol, write tag and move on
+                    writeTag("symbol", currentToken);
+                    readingComment = false;
+                    currentToken = tokenType = "";
+                    writeToken = checkNext = false;
+                }
+            }
+
+            /***********************************************************************************************************
+             * Constants, keywords, ids                                                                               *
+             ***********************************************************************************************************/
+
+            // New tag, find out what it is
+            if(tokenType.equals("")) {
+                switch (lexeme) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        tokenType = "integerConstant";
+                        currentToken += lexeme;
+                        continue;
+                    case '"' :
+                        tokenType = "stringConstant";
+                        continue;
+                    case '\t' :
+                    case ' ' :
+                        // No need to do anything, move on
+                        continue;
+                    case '_' :
+                        tokenType = "identifier";
+                        currentToken += lexeme;
+                        continue;
+                    default:
+                        if(symbols.contains(lexeme)) {
+                            tokenType = "symbol";
+                            currentToken += lexeme;
+                            writeToken = true;
+                            break;
+                        }
+                        currentToken += lexeme;
+                        tokenType = "UNKNOWN"; // We don't know yet
+                }
+            } else {
+                switch (lexeme) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        currentToken += lexeme;
+                        continue;
+                    case '"' :
+                        writeToken = true;
+                        break;
+                    case '\t' :
+                    case ' ' :
+                        // If working on string constant
+                        if(tokenType.equals("stringConstant")) {
+                            currentToken += lexeme;
+                            continue;
+                        }
+
+                        // Lookup if reserved word
+                        if(reservedWords.contains(currentToken))
+                            tokenType = "keyword";
+                        else
+                            tokenType = "identifier";
+
+                        writeToken = true;
+                        break;
+                    default:
+                        if(tokenType.equals("stringConstant")) {
+                            currentToken += lexeme;
+                            continue;
+                        }
+
+                        // Handle symbol next to other
+                        if(symbols.contains(lexeme)) {
+                            // Lookup if reserved word
+                            if(reservedWords.contains(currentToken))
+                                tokenType = "keyword";
+                            else if(!tokenType.equals("integerConstant"))
+                                tokenType = "identifier";
+
+                            // Dump what we have
+                            writeTag(tokenType, currentToken);
+                            // Write Symbol
+                            writeTag("symbol", String.valueOf(lexeme));
+                            tokenType = currentToken = "";
+                            writeToken = false;
+                            continue;
+                        }
+
+                        if(!tokenType.equals("integerConstant"))
+                            currentToken += lexeme;
+                        else
+                            writeToken = true;
+                }
+            }
+
+            if(writeToken) {
+                writeTag(tokenType, currentToken);
+                tokenType = currentToken = "";
+                writeToken = false;
+            }
+        }
+    }
+
     /**
      * Writes the given tag and value
      * @param tagName Name for xml tag
@@ -115,7 +272,6 @@ public class JackTokenizer {
      */
     private static void writeTag(String tagName, String value) {
         // Open tag
-        writeBuffer.add(' ');
         writeBuffer.add('<');
         // Tag itself
         for(Character letter : tagName.toCharArray())
@@ -125,8 +281,14 @@ public class JackTokenizer {
 
         // Write the value
         writeBuffer.add(' ');
-        for(Character letter : value.toCharArray())
-            writeBuffer.add(letter);
+        for(Character letter : value.toCharArray()) {
+            if(letter == '<') {
+                for(Character temp : "&lt;".toCharArray())
+                    writeBuffer.add(temp);
+            }
+            else
+                writeBuffer.add(letter);
+        }
         writeBuffer.add(' ');
 
         // Close tag
